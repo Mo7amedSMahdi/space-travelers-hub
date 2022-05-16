240014f8a3c037f31e20eba3185cbab8629c55b4
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-05-16 15:37:40 +0300

    install code linters

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h1>Welcome!</h1>
+  </div>
+);
 
 export default App;
