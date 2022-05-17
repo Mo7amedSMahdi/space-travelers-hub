4ba5306a1321d0a3ad2a7da32666853ec6144b19
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-05-17 17:15:41 +0300

    fetch missions from server using middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
+// import thunk from 'redux-thunk';
+import api from './middleware/api';
 import missionsReducer from './missions';
 
 const rootReducer = combineReducers({ missions: missionsReducer });
 
-const store = configureStore({ reducer: rootReducer });
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [
+    ...getDefaultMiddleware(),
+    // logger({ destination: "console" }),\
+    api,
+  ],
+});
 
 export default store;
