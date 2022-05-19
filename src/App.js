aaaf11285f1ffb0334f0c4efee16959a603765f0
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-05-19 11:00:03 +0300

    fix the eslint errors

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ import store from './store/configureStore';
 import Mission from './components/mission/Mission';
 import Profile from './components/profile/Profile';
 import Navbar from './components/header/Navbar';
-import { Routes, Route } from "react-router-dom";
-import "./assets/css/global.scss";
-import Rocket from "./pages/rockets";
+import './assets/css/global.scss';
+import Rocket from './pages/rockets';
 
 const App = () => (
-  <div className="App">
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Rocket />} />
-      <Route path="/mission" element={<Mission />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
-  </div>
+  <Provider store={store}>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Rocket />} />
+        <Route path="/mission" element={<Mission />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </div>
+  </Provider>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
-/* eslint-disable comma-dangle */
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import store from "./redux/configureStore";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </Router>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
